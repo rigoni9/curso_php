cffe1655303e72d0a3fe77ca343475a08d49db98
rigoni9 <rigoni9@hotmail.com>
2024-12-17 20:23:13 -0300

    Create calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * operadores (+ - * /)
+*/
+
+$x = 8;
+$y = 10;
+$operador = "*";
+
+if ($operador == "+") {
+    $soma = $x + $y;
+    echo "A função é $x + $y = $soma";
+} else if ($operador == "-") {
+    echo "A função é $x - $y = " . ($x - $y);
+} else if ($operador == "*") {
+    echo "A função é $x * $y = " . ($x * $y);
+} else if ($operador == "/") {
+    echo "A função é $x / $y = " . ($x / $y);
+} 
