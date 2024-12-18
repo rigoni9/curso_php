54265d33d1c5d64ac488afdc586ccebcc28dcde6
rigoni9 <rigoni9@hotmail.com>
2024-12-17 21:03:50 -0300

    up

--- calculadora.php
+++ calculadora.php
@@ -2,11 +2,12 @@
 
 /**
  * operadores (+ - * /)
+ * # = percentual
 */
 
 $numero1 = 8;
 $numero2 = 10;
-$operador = "+";
+$operador = "#";
 
 if ($operador == "+") {
     $soma = $numero1 + $numero2;
@@ -17,6 +18,8 @@ if ($operador == "+") {
     echo "A função é $numero1 * $numero2 = " . ($numero1 * $numero2);
 } else if ($operador == "/") {
     echo "A função é $numero1 / $numero2 = " . ($numero1 / $numero2);
+} else if ($operador == "#") {
+    echo "$numero2% de $numero1 é " . ($numero1 * ($numero2/100));
 } else {
     echo "Operador inválido!<br> Use apenas + - * /";
 }
