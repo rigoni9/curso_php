2e50b34ce78b79bd155b6b7d3577281c48679dba
rigoni9 <rigoni9@hotmail.com>
2025-01-07 20:18:58 -0300

    up

--- calendario.php
+++ calendario.php
@@ -5,11 +5,13 @@
  * Validar a data.
  * Validar se o ano é bissexto.
  * Dica: If else, 
+ * $mes = (int) $mes - Converte para um número inteiro
  */
 // 31 / 01 / 2025
 // 01 2 34 5 6789
 
- $data = "31/01/2025";
+
+ $data = "29/01/2100";
 
  $dia = substr($data, 0, 2); //primeiro número é a casa, segundo número é a quantidade de casas.
  $mes = substr($data, 3, 2);
