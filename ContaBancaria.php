785fd51d1df5d7a30a2f825e4e2762ea5259db29
Rigoni9 <rigoni9@hotmail.com>
2025-01-21 21:48:00 -0300

    up

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -1,10 +1,34 @@
 <?php
 
 class ContaBancaria {
+    public $titular = "";
+    public $saldo = 0;
+    public $destinatario = "";
+    public $historico = [];
+
     public function sacar() {}
     public function depositar() {}
     public function verSaldo() {}
     public function pix() {}
-    public function extrato() {}
+    public function extrato() {
+        $this->historico = [
+            "odair" => [
+                "ariel" => 20.50
+            ],
+            "ariel" => [
+                "diego" => 50
+            ]
+            ];
+
+            foreach($this->historico as $titular => $destinatarios) {
+                foreach($destinatarios as $destinatario => $valor) {
+                    echo "Titular: $titular Destinatário: $destinatario Valor: R$$valor.<br>";
+                }
+            }
+    }
+
 
 }
+
+$conta = new ContaBancaria();
+echo $conta->extrato();
