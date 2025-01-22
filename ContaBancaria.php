bbe25f8b8b45a0ba7aa2732d14fafedab1d0b6ed
Rigoni9 <rigoni9@hotmail.com>
2025-01-21 21:32:02 -0300

    up

---
+++ ContaBancaria.php
@@ -0,0 +1,10 @@
+<?php
+
+class ContaBancaria {
+    public function sacar() {}
+    public function depositar() {}
+    public function verSaldo() {}
+    public function pix() {}
+    public function extrato() {}
+
+}
