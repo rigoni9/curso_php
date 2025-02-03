e49b5068a82ce3747480bc494a563287d1f552e3
Diego Rigoni <rigoni9@hotmail.com>
2025-02-03 19:02:22 -0300

    Update ContaBancaria.php

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -1,34 +1,104 @@
 <?php
 
+require_once "./ArquivoTxt.php";
+
 class ContaBancaria {
     public $titular = "";
-    public $saldo = 0;
     public $destinatario = "";
+    public $saldo = 0;
     public $historico = [];
+    
+    private function temSaldo($valor) {
 
-    public function sacar() {}
-    public function depositar() {}
-    public function verSaldo() {}
-    public function pix() {}
-    public function extrato() {
-        $this->historico = [
-            "odair" => [
-                "ariel" => 20.50
-            ],
-            "ariel" => [
-                "diego" => 50
-            ]
-            ];
+        if ($valor <= $this->saldo) {
+            return true;
+        }
 
-            foreach($this->historico as $titular => $destinatarios) {
-                foreach($destinatarios as $destinatario => $valor) {
-                    echo "Titular: $titular Destinatário: $destinatario Valor: R$$valor.<br>";
-                }
-            }
+        return false;
     }
 
+    public function sacar($valor) {
+        $temSaldo = $this->temSaldo($valor);
 
+        if (!$temSaldo) {
+            throw new Exception("Erro! Saldo atual R$: $this->saldo. Valor à sacar:  R$ $valor.");
+        }
+
+        $this->saldo -= $valor; // $this->saldo = $this->saldo - $valor;
+        $this->setHistorico($this->titular, $this->saldo);
+    }
+
+    public function depositar($valor) {
+
+        if ($valor <= 0) {
+            throw new Exception("Erro! Valor invalido. R$ $valor.");
+        }
+
+        $this->saldo += $valor;
+        $this->setHistorico($this->titular, $this->saldo, "Deposito");
+    }
+
+    public function pix($valor) {
+
+        $temSaldo = $this->temSaldo($valor);
+
+        if (!$temSaldo) {
+           throw new Exception("Erro ao efetuar o PIX.<br> Saldo atual R$: $this->saldo. valor a transferir R$ $valor.");
+        }
+
+        $this->saldo -= $valor;
+        $this->setHistorico("Ana", $this->saldo, "Maria");
+    }
+
+    private function setHistorico($titular, $valor, $destinatario = "Saque") {
+        $arrDestinatario = [
+            $destinatario => $valor,
+        ];
+
+        $arrTitular = [
+            $titular => $arrDestinatario,
+        ];
+
+        $this->historico[] = $arrTitular;
+    }
+
+    public function extrato() {
+        // $this->historico = [
+        //     "odair" => [
+        //         "ariel" => 20.50
+        //     ],
+        //     "ariel" => [
+        //         "diego" => 50
+        //     ]
+        // ];
+
+        for($i=0; $i < count($this->historico); $i++) {
+            foreach($this->historico[$i] as $titular => $destinatarios) {
+                foreach($destinatarios as $destinatario => $valor) {
+                    echo "Titular: $titular Destinatario: $destinatario valor: R$ $valor.<br>";
+                }
+            }
+        }
+
+    }
 }
 
-$conta = new ContaBancaria();
-echo $conta->extrato();
+// $conta = new ContaBancaria();
+// $conta->depositar(500);
+// $conta->sacar(100);
+// $conta->pix(200);
+// echo $conta->extrato();
+
+$caminhoArquivo = "C://users//aluno//documents//";
+$nomeArquivo = "banco_do_brasil.txt";
+$arquivo = $caminhoArquivo . $nomeArquivo;
+
+$arquivoTxt = new ArquivoTxt($nomeArquivo);
+$conta = [
+    "id" => 10,
+    "nome" => "Joao",
+    "saldo" => 100,
+];
+
+$arquivoTxt->escrever($conta);
+echo $arquivoTxt->ler();
