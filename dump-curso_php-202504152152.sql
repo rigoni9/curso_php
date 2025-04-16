9177940dfbd165e6f2a77034fb10bc032d1d4e17
Diego Rigoni <rigoni9@hotmail.com>
2025-04-15 21:53:18 -0300

    Add files via upload

---
+++ dump-curso_php-202504152152.sql
@@ -0,0 +1,153 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: curso_php
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.18-MariaDB-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `conta_bancaria`
+--
+
+DROP TABLE IF EXISTS `conta_bancaria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conta_bancaria` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `nome_titular` varchar(255) NOT NULL,
+  `saldo` decimal(10,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conta_bancaria`
+--
+
+LOCK TABLES `conta_bancaria` WRITE;
+/*!40000 ALTER TABLE `conta_bancaria` DISABLE KEYS */;
+INSERT INTO `conta_bancaria` VALUES (1,'Diego',200.00),(2,'Ariel',500.00),(3,'Odair',800.00),(4,'Diogo',1100.00),(5,'Rafael',1400.00);
+/*!40000 ALTER TABLE `conta_bancaria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enderecos`
+--
+
+DROP TABLE IF EXISTS `enderecos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enderecos` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `cep` varchar(8) NOT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `logradouro` varchar(255) NOT NULL,
+  `numero` varchar(20) DEFAULT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `estado` varchar(100) NOT NULL,
+  `estado_sigla` varchar(2) NOT NULL,
+  `pais` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enderecos`
+--
+
+LOCK TABLES `enderecos` WRITE;
+/*!40000 ALTER TABLE `enderecos` DISABLE KEYS */;
+INSERT INTO `enderecos` VALUES (1,'95700000','Santa Marta','Rua Wosvaldo Cruz','369','Prédio','Bento Gonçalves','Rio Grande do Sul','RS','Brasil'),(2,'83745000','Centro','Rua Vitório Carraro','258','Casa','Garibaldi','Santa Catarina','SC','Brasil');
+/*!40000 ALTER TABLE `enderecos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoas`
+--
+
+DROP TABLE IF EXISTS `pessoas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoas` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `sobrenome` varchar(255) NOT NULL,
+  `celular` varchar(15) DEFAULT NULL,
+  `data_nascimento` date NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `genero` enum('M','F') DEFAULT NULL,
+  `estado_civil` enum('solteiro','casado','viuvo','separado') DEFAULT NULL,
+  `nome_mae` varchar(255) NOT NULL,
+  `cor` varchar(255) DEFAULT NULL,
+  `raca` varchar(255) DEFAULT NULL,
+  `nacionalidade` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `id_usuarios` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pessoas_usuarios_FK` (`id_usuarios`),
+  CONSTRAINT `pessoas_usuarios_FK` FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoas`
+--
+
+LOCK TABLES `pessoas` WRITE;
+/*!40000 ALTER TABLE `pessoas` DISABLE KEYS */;
+INSERT INTO `pessoas` VALUES (1,'diego','rigoni','54999154853','1991-04-18','83777083020','M','casado','justina','branco','humana','brasileiro',1,1),(2,'diogo','diogo','54999154853','1991-04-18','83777083020','M','casado','ana','branco','humana','brasileiro',1,2),(3,'odair','odair','54999154853','1991-04-18','83777083020','M','casado','beta','branco','humana','brasileiro',1,3),(4,'ariel','ariel','54999154853','1991-04-18','83777083020','M','casado','manu','branco','humana','brasileiro',1,4);
+/*!40000 ALTER TABLE `pessoas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `nome_usuario` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `senha` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'diego','diego@infoserv.com.br',1,'1234'),(2,'antonio','antonio@infoserv.com.br',1,'1256'),(3,'sandi','sandi@infoserv.com.br',1,'2234'),(4,'junior','junior@infoserv.com.br',1,'as34'),(5,'rafael','rafael@inforserv.com',1,'4456');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'curso_php'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-15 21:52:44
